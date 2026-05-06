12e270585ce5668080cee22ff0f48aa49242f463
npk <nakul@faircodetech.com>
2026-05-06 18:00:03 +0530

    chore: remove unwanted scratch and utility scripts

--- erpnext_scrum/check_types.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def check_task_types():
-    types = frappe.get_all("Task Type", pluck="name")
-    print(f"Available Task Types: {types}")
-
-if __name__ == "__main__":
-    check_task_types()
